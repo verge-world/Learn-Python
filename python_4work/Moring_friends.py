8411009a9c354a67eba3be16c82208ef150a8d2e
verge-world <hnh1512080@163.com>
2025-03-09 11:50:20 +0800

    Delete python_4work/Moring_friends.py

--- python_4work/Moring_friends.py
+++
@@ -1,9 +0,0 @@
-names = ['Jame','Lucy','Jcky','Petter']
-message = f"{names[0]},good moring!"
-print(message)
-message = f"{names[1]},good moring!"
-print(message)
-message = f"{names[2]},good moring!"
-print(message)
-message = f"{names[-1]},good moring!"
-print(message)
